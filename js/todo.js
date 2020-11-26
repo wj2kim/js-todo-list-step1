178e636e57db0f4b68b85f45fe9623e1e5800e52
김우정 <wjkim@rsupport.com>
2020-11-26 23:04:35 +0900

    투두 리스트 삭제

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,6 @@
 const $list = document.querySelector('#todo-list');
 const $newTodoTitle  = document.querySelector('#new-todo-title');
-let id = 0;
+let id = 1;
 
 /**
  * 투두 저장소
@@ -9,6 +9,7 @@ const Todos = () => {
     let todos = [
         { id: 1, title: "투두리스트 만들기", done: false},
     ];
+
     return {
         selectAll(){
             return [...todos];
@@ -17,6 +18,8 @@ const Todos = () => {
             return todos = [];
         },
         insertOne(todo = {}){
+            console.log(!!todo)
+
             if(todo){
                 todos.push(todo);
                 return true;
@@ -42,6 +45,34 @@ const Todos = () => {
 
 const todosStore = Todos(); 
 
+const addEvents = () => {
+    const $toggle = document.querySelectorAll('.toggle');
+    $toggle.forEach(toggle => toggle.addEventListener('click', e => {
+        console.log("e.target checked : ", e.target.checked);
+        const checked  = e.target.checked;
+        if(checked){
+            toggle.parentNode.parentNode.classList.add('completed');
+            toggle.setAttribute('checked', "");
+        }else{
+            toggle.parentNode.parentNode.classList.remove('completed');
+            toggle.removeAttribute('checked');
+        }
+    }))
+
+  
+
+    const destroyTodoItem = (id) => {
+       console.log("delete id", id)
+        
+    }
+
+    return {
+        destroyTodoItem: destroyTodoItem
+    }
+}
+
+
+
 /**
  * 투두 추가 함수
  * @param {*} todo 
@@ -50,16 +81,18 @@ const addTodo = (todo = {}) => {
     if(!todo){
         return;
     }
+
     const todoInHTML = `<li id=${todo.id} class="todoItem">
-                            <div class="view>
-                                <input class="toggle" type="checkbox" />
+                            <div class="view">
+                                <input class="toggle" type="checkbox"/>
                                 <label class="lable">${todo.title}</label>
-                                <button class="destory"></button>
+                                <button class="destroy" onclick="addEvents().destroyTodoItem(${todo.id})"></button>
                             </div>
                             <input class="edit" value=${todo.title} />
                         </li>`
 
     $list.insertAdjacentHTML('beforeend', todoInHTML);
+    addEvents();
 }
 
 
@@ -67,17 +100,29 @@ const addTodo = (todo = {}) => {
  * 투두 리스트 페이지 첫 진입 시 
  */
 const init = () => {
+    // todo : 로컬스토리지에서 저장된 데이터 가지고 오는 로직 
+   
     const initTodoList = () => {
         const todos = todosStore.selectAll();
         todos.map(todo => addTodo(todo));
     }
 
-    // todo : 로컬스토리지에서 저장된 데이터 가지고 오는 로직 
-    
     initTodoList();
 }
 
 init();
+
+// eventlist 추가 함수
+
+const validateTitle = (title = "") => {
+    if(!title){
+        return false;
+    }
+
+    const blank_pattern = /^\s+|\s+$/g;
+
+    return title.replace(blank_pattern, "").length > 0 ? true :  false;
+}
 
 
 /**
@@ -87,15 +132,17 @@ $newTodoTitle.addEventListener('keyup', e => {
     // keyCode 가 deprecated?
     if(e.code === 'Enter'){
         const title = e.target.value;
-        if(title){
+        const isValidate = validateTitle(title);
+        if(isValidate){
             const todo = {
                 id: id += 1,
-                title,
+                title: e.target.value,
                 done: false,
             }
             todosStore.insertOne(todo) && addTodo(todo);
         }
         $newTodoTitle.value = "";
+       
     }
 })
 
